5cd09f4761ea2282d94b210cbf8781ac9b246bb9
maduiep <emmanuelmaduip@gmail.com>
2020-05-10 14:15:23 +0100

    fourth commit

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -9,7 +9,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
  const db = client.db('TodoApp')
 
 
-
+ /*
  db.collection('Todos').insertOne({
   text: 'Eaten Sunday Today',
   completed: true
@@ -19,33 +19,34 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   }
   console.log(JSON.stringify(result.ops, undefined, 2))
  })
- /*
-  Insert new doc into Users {name, age, location}
- 
-  
-   db.collection('Users').insertOne({
-    name: 'Emmanuel Madu',
-    age: 27,
-    location: 'Enugu, Nigeria.'
-   }, (err, result) => {
-    if (err) {
-     return console.log('Unable to insert Todo', err)
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2))
-   })
-  
- 
-  db.collection('Users').insertOne({
-   name: 'Jasmine Egbuna',
-   age: 24,
-   location: 'Abuja, Nigeria.'
-  }, (err, result) => {
-   if (err) {
-    return console.log('Unable to insert Todo', err)
-   }
-   console.log(JSON.stringify(result.ops, undefined, 2))
-  })
- */
+  */
+
+ //Insert new doc into Users {name, age, location}
+
+
+ db.collection('Users').insertOne({
+  name: 'Paschal Nwankwo',
+  age: 27,
+  location: 'Abuja, Nigeria.'
+ }, (err, result) => {
+  if (err) {
+   return console.log('Unable to insert Todo', err)
+  }
+  console.log(JSON.stringify(result.ops, undefined, 2))
+ })
+
+
+ db.collection('Users').insertOne({
+  name: 'Justa Nwonu',
+  age: 24,
+  location: 'Enugu, Nigeria.'
+ }, (err, result) => {
+  if (err) {
+   return console.log('Unable to insert Todo', err)
+  }
+  console.log(JSON.stringify(result.ops, undefined, 2))
+ })
+
 
  client.close()
 })
--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -8,6 +8,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
  console.log('Connected to MongoDB server')
  const db = client.db('TodoApp')
 
+ // Delete for Todos
  // deleteMany
  db.collection('Todos').deleteMany({ text: 'Eaten Sunday Today' }).then((result) => {
   console.log(result)
@@ -20,6 +21,23 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
  // findOneAndDelete
  db.collection('Todos').findOneAndDelete({ completed: false }).then((result) => {
   console.log(result)
+ })
+
+ // Delete for Users
+ // deleteMany
+ db.collection('Users').deleteMany({ name: 'Justa Nwonu' }).then((results) => {
+  console.log(results)
+ })
+ // deleteOne
+ db.collection('Users').deleteOne({ name: 'Paschal Nwankwo' }).then((results) => {
+  console.log(JSON.stringify(results, undefined, 2))
+ })
+
+ // findOneAndDelete
+ db.collection('users').findOneAndDelete({
+  _id: new ObjectID("5eb7fcf8b3bd2e1044e1ee2d")
+ }).then((results) => {
+  console.log(JSON.stringify(results, undefined, 2))
  })
 
  //client.close()
