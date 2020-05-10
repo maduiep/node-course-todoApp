e1909f117dcaa1c034531e0cde5b8efeb74b4fb7
maduiep <emmanuelmaduip@gmail.com>
2020-05-10 21:12:04 +0100

    adding post/todos

--- server/server.js
+++ server/server.js
@@ -1,4 +1,24 @@
-const {mongoose} = require('./db/mongoose')
+const express = require('express')
+const bodyParser = require('body-parser')
 
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+
+const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
+
+app.post('/todos', (req, res) => {
+ var todo = new Todo({
+  text: req.body.text
+ })
+
+ todo.save().then((doc) => {
+  res.send(doc)
+ }, (e) => {
+  res.status(400).send(e)
+ })
+})
+app.listen(port, () => console.log(`Server app listening on port ${port}`))
